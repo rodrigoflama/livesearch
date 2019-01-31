114d4a407018eecbbacc5972e8adca5cbcab4865
rodrigoflama <rodrigo.flama@gmail.com>
2019-01-30 22:36:49 -0200

    ajustes no js

---
+++ app/assets/javascripts/contacts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("#contacts_search input").keyup(function() {
+    $.get($("#contacts_search").attr("action"), $("#contacts_search").serialize(), null, "script");
+    return false;
+  });
+});
